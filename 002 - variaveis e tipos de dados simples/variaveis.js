ae81bca45248ca71f2ddc7c97ad7c80ba952deee
Juliana Souza <juliana313031@gmail.com>
2022-06-07 17:25:21 -0300

    finalizei os estudos das variaveis

---
+++ 002 - variaveis e tipos de dados simples/variaveis.js
@@ -0,0 +1,7 @@
+var idade = 10 // isso é uma variavel do tipo int
+var nome = "juma" // isso é uma variavel do tipo string 
+var peso = 2.5 // essa é uma variavel do tipo float 
+var estudante = true // essa é uma variavel do tipo boolean
+console.log(`Seu nome é ${nome}, sua idade é ${idade} anos, seu peso é ${peso} kilos e a opção para estudante é ${estudante}`)
+var nascimento = 1989
+console.log(`${nome} sua idade é ${2022 - nascimento} anos`)
